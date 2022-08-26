2233f053a8e011b1a708b18ef6257b3078d7e428
Amory Griffith <nguyenchingo28@gmail.com>
2022-08-26 21:52:01 +0700

    Setting up project and organise files

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Components from '../components';
+export default function MainScreen() {
+  return (
+    <div>
+      <Components.Header />
+      <Components.Hero />
+      <Components.Carousel />
+      <Components.Footer />
+    </div>
+  );
+}
